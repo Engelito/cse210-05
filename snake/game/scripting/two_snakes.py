048b8f5e64236eec0668036e7330803c5b7809c3
ChrIstoPher339 <97273563+ChrIstoPher339@users.noreply.github.com>
2022-03-12 12:42:55 -0700

    add two_snakes.py

---
+++ snake/game/scripting/two_snakes.py
@@ -0,0 +1,20 @@
+from game.scripting.handle_collisions_action import HandleCollisionsAction
+
+class SecondSnake(HandleCollisionsAction):
+    def _handle_segment_collision(self, cast):
+        snake1 = cast.get_first_actor("snake1")
+        snake2 = cast.get_first_actor("snake2")
+        
+        head1 = snake1.get_segments()[0]
+        segments1 = snake1.get_segments()[1:]
+
+        head2 = snake2.get_segments()[0]
+        segments2 = snake2.get_segments()[1:]
+        
+        for segment in segments1:
+            if head1.get_position().equals(segment.get_position()):
+                self._is_game_over = True
+        
+        for segment in segments2:
+            if head2.get_position().equals(segment.get_position()):
+                self._is_game_over = True
